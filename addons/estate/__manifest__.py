0f6752fc4e11ad45e067a64b78f4d14427dbd048
syamban14 <syamsulikj.48@gmail.com>
2025-05-26 11:40:49 +0700

    Add depends on manifest

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -2,6 +2,9 @@
     'name': "Real Estate Management",
     'version': '1.0',
     'category': 'Real Estate',
+    'depends': [
+        'base'
+        ],
     'summary': 'Manage real estate properties, listings, and transactions',
     'description': """
         This module provides functionalities to manage real estate properties, including listings, transactions, and client interactions.
